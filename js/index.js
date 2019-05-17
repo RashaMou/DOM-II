4484b489a5c804ba61e09ad495714ae0fa716499
Rasha Moumneh <r.moumneh@gmail.com>
2019-05-17 19:55:09 -0400

    load event triggers alert that you really need a vacation

--- js/index.js
+++ js/index.js
@@ -23,12 +23,12 @@ console.log(headers);
 
 headers.forEach(function(header){
     header.addEventListener('mouseover', function(){
-        header.style.marginLeft = "20%";
+        header.style.marginLeft = "30%";
         header.style.color = "orange";
     })
 });
 
-// 3. wheel
+// 3. wheel event changes text background color and gives it a roundover and padding
 
 const text = document.querySelectorAll("p");
 const page = document.querySelector("html"); 
@@ -39,5 +39,13 @@ page.addEventListener('wheel', function(){
         para.style.borderRadius = "8px"
         para.style.padding = "10px";
     })
-})
+});
+
+//4. load event triggers alert that you really need a vacation
+
+window.addEventListener('load', (event) => {
+    alert('You REALLY need a vacation');
+});
+
+
 
