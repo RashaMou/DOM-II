28155692c6075ea6a9f14cf878ed35b2b1e89156
Rasha Moumneh <r.moumneh@gmail.com>
2019-07-13 00:18:50 -0400

    implement block move on mousedown and return to original place on mouseup

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -5,14 +5,24 @@ const blocks = document.querySelectorAll('.block');
 // add evemt listener to each block
 // on click, loop through all the blocks to check if that block was clicked. If it was, give it order of -1. 
 // otherwise, blocks get order of 0
-blocks.forEach(block => block.addEventListener('click', function(event) {
- blocks.forEach(block => {
-   if (block == event.target) {
-    event.target.style.order = "-1";
-   } else {
-    block.style.order = "0";
-   }
-  })
+// blocks.forEach(block => block.addEventListener('click', function(event) {
+//  blocks.forEach(block => {
+//    if (block == event.target) {
+//     event.target.style.order = "-1";
+//    } else {
+//     block.style.order = "0";
+//    }
+//   })
+// }));
+
+blocks.forEach(block => block.addEventListener('mousedown', (e) => {
+  e.target.classList.add('animate');
+  console.log("down");
+}));
+
+blocks.forEach(block => block.addEventListener('mouseup', (e) => {
+  e.target.classList.remove('animate');
+  console.log("down");
 }));
 
 
